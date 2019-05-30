ac93f46e8eca6cc6fac0eaff71d8d41b98525f30
xuzechao <xuzechao12@163.com>
2019-05-30 10:54:02 +0800

    q

--- spring-boot-mybatis/spring-boot-mybatis-annotation/src/main/java/com/neo/MybatisAnnotationApplication.java
+++ spring-boot-mybatis/spring-boot-mybatis-annotation/src/main/java/com/neo/MybatisAnnotationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.neo.mapper")
 public class MybatisAnnotationApplication {
-
+//sssssssssssssssss
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisAnnotationApplication.class, args);
 	}
